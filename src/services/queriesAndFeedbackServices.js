1268994392ade7cf963ce48fbf3580737de9ec49
Kirti Shukla <shuklakirti2004@gmail.com>
2026-02-20 03:20:27 +0530

    fixed gettoekn bug

--- src/services/queriesAndFeedbackServices.js
+++ src/services/queriesAndFeedbackServices.js
@@ -1,4 +1,3 @@
-import { getToken } from "./authStorage";
 const token = localStorage.getItem("jobtracker_token");
 const API_URL = process.env.REACT_APP_API_BASE_URL_FEEDBACK;
 export const submitFeedback = async ({ comment }) => {
